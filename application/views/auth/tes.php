5880868e94828dfd9d0da3fc437312b8cb9bcbb4
fakhri54 <fakhrinukman54@gmail.com>
2022-04-24 01:31:11 +0700

    first commit

---
+++ application/views/auth/tes.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title><?= $tittle ?></title>
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="<?= base_url('assets/') ?>vendor/css/styles.css" rel="stylesheet" />
+    <link href="<?= base_url('assets/') ?>vendor/css/yess.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+        <div class="container px-4 px-lg-5">
+            <a class="navbar-brand" href="#page-top"><?= $tittle ?></a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="#about2">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+    <header class="masthead" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 75%, #000 100%), url(<?= base_url('assets/img/bg-masthead.jpg') ?>);">
+        <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+            <div class="d-flex justify-content-center">
+                <div class="text-center">
+                    <h1 class="mx-auto my-0 text-uppercase">Sthira</h1>
+                    <h2 class="text-white-50 mx-auto mt-2 mb-5">Kami merupakan Team Web Designer dari Universitas Syiah Kuala.</h2>
+                    <a class="btn btn-primary" href="#about">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- About-->
+    <section class="about-section" style="background-image: linear-gradient(to bottom, #000 0%, rgba(0, 0, 0, 0.9) 20%, rgba(0, 0, 0, 0.8) 100%), url(<?= base_url('assets/img/about-bg.jpg') ?>)" id="about">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-8">
+                    <h2 class="text-white text-center mb-4">Apa itu Sthira ?</h2>
+                    <p class="text-white-50 text-center">
+                        Sthira merupakan salah satu team design yang dibentuk pada tahun 2020. untuk memenuhi tugas dari mata kuliah design . Nama Sthira sendiri diambil dari bahasa jawa kuno sendiri yang bermakna Kuat, Tabah, Tepercaya dan dapat diandalkan <br>
+                        Kami berharap team kami sesuai dengan arti/makna dari sthira tersebut.
+                    </p>
+                </div>
+            </div>
+            <div class="text-center">
+                <img class="img-fluid" src="assets/img/logo_sthira.png" alt="..." />
+            </div>
+        </div>
+    </section>
+    <!-- about-patner sthira -->
+
+
+
+    <!-- Projects-->
+
+    <!-- Contact-->
+    <section class="contact-section bg-black">
+        <div class="container px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5">
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <div class="card py-4 h-100">
+                        <div class="card-body text-center">
+                            <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                            <h4 class="text-uppercase m-0">Address</h4>
+                            <hr class="my-4 mx-auto" />
+                            <div class="small text-black-50">Jl. Dr. Mr. Mohd Hasan
+                                Batoh
+                                Kec. Lueng Bata
+                                Kota Banda Aceh
+                                Aceh</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <div class="card py-4 h-100">
+                        <div class="card-body text-center">
+                            <i class="fas fa-envelope text-primary mb-2"></i>
+                            <h4 class="text-uppercase m-0">Email</h4>
+                            <hr class="my-4 mx-auto" />
+                            <div class="small text-black-50"><a href="https://mail.google.com/mail/u/0/?tab=km#inbox?compose=DmwnWstwPJttcdxKRFRDqgvfMFBGbtPFjpWxQpjsrPdvgMFsxcswCFkSNFkrQFHsKfgdQhVSCxrq">sthira.team30@gmail.com</a></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3 mb-md-0">
+                    <div class="card py-4 h-100">
+                        <div class="card-body text-center">
+                            <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                            <h4 class="text-uppercase m-0">Phone</h4>
+                            <hr class="my-4 mx-auto" />
+                            <div class="small text-black-50">+62 823 6090 8254</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="social d-flex justify-content-center">
+                <a class="mx-2" href="https://id.pinterest.com/sthirateam/"><i class="fa-brands fa-pinterest-p"></i></i></a>
+                <a class="mx-2" href="https://www.instagram.com/sthira.team30/"><i class="fa-brands fa-instagram"></i></i></a>
+                <a class="mx-2" href="https://www.behance.net/sthirateam"><i class="fa-brands fa-behance"></i></i></a>
+            </div>
+        </div>
+    </section>
+    <!-- Footer-->
+    <footer class="footer bg-black small text-center text-white-50">
+        <div class="container px-4 px-lg-5">Copyright &copy; Sthira Website <?= date('Y'); ?></div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="<?= base_url('assets/') ?>vendor/js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
